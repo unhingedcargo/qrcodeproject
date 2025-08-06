70f2c894a20a951d9e1e6e9a433a0d03a02d6492
unhingedcargo <unhingedcargo@gmail.com>
2025-08-06 19:58:58 +0530

    Ofc-06Aug

--- static/js/scriptContact.js
+++ static/js/scriptContact.js
@@ -20,6 +20,42 @@ allInput.forEach(input => {
 });
   
 document.getElementById('create').addEventListener('click', (e) => {
-  console.log(dataMap);
+  // console.log(dataMap.get('firstName'));
+  // dataMap.forEach(entry => {
+
+  //   if(entry){console.log(entry.length)};
+  // });
+  let firstName="", lastName="", title="", phone="", mobile="", emailContact="", website="", organisation="";
+  if(dataMap.get('firstName')){firstName = dataMap.get('firstName')}
+  if(dataMap.get('lastName')){lastName = dataMap.get('lastName')}
+  if(dataMap.get('title')){title = dataMap.get('title')}
+  if(dataMap.get('organisation')){street = dataMap.get('organisation')}
+  if(dataMap.get('phone')){phone = dataMap.get('phone')}
+  if(dataMap.get('mobile')){mobile = dataMap.get('mobile')}
+  if(dataMap.get('emailContact')){emailContact = dataMap.get('emailContact')}
+  if(dataMap.get('website')){firstName = dataMap.get('website')}
+
+  const vcardData = `BEGIN:VCARD
+  VERSION:3.0
+  N:${lastName};${firstName};;${title};
+  FN:${firstName} ${lastName}
+  ORG: ${organisation}
+  TEL;TYPE=WORK,MSG:${phone}
+  TEL;TYPE=WORK,MOBILE:${mobile}
+  EMAIL;TYPE=INTERNET:${emailContact}
+  URL: ${website}
+  END:VCARD`;
+
+  console.log(vcardData);
+  const qrDiv = document.getElementById("contact-qr");
+  qrDiv.innerHTML = "";
+
+  new QRCode(qrDiv, {
+              text: vcardData,
+              width: 200,
+              height: 200,
+              render: "canvas"
+          });
+
 });
 
